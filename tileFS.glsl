ceb4f304879f45dab029c488e86e510457d46c13
Olivier Guyot <olivier.guyot@camptocamp.com>
2018-07-04 10:33:15 +0200

    added vector tiles example

--- tileFS.glsl
+++
@@ -1,9 +0,0 @@
-precision mediump float;
-
-varying vec2 v_texCoord;
-uniform sampler2D u_texture;
-
-void main(void) {
-  vec4 texColor = texture2D(u_texture, v_texCoord);
-  gl_FragColor.rgb = texColor.rgb;
-}
